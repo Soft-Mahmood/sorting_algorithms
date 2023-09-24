3db4d4c6a275080dd85748feb4c687740cd74acd
Ernest-Geek <ernestampene1@gmail.com>
2023-09-23 20:09:22 -0700

    quick fix

--- Ernest/0-bubble_sort.c
+++ Ernest/0-bubble_sort.c
@@ -1,26 +1,6 @@
 #include "sort.h"
 #include <stdlib.h>
 /**
- * Code description: Prints the array after each swap
- * bubble_sort - it sorts an array of integers in ascending order
- * @array: An array of integers to be sorted
- * @size: Which is the size of the array
-*/
-void bubble_sort(int *array, size_t size)
-{
-size_t x, y;
-for (x = 0; x < size - 1; x++)
-{
-for(y = 0; y < size - 1 - x; y++)
-{
-if (array[x] > array[y + 1])
-{
-exchange_int(&array[x], &array[y+1]);
-}
-}
-}
-}
-/**
 **
  * exchange_int - Which is used to exhange the integers
  * @xy: A pointer to the first integer to swap
@@ -35,4 +15,27 @@ int temp = *xy;
 *xy = *yp;
 /**Updates the value pointed to by yp**/
 *yp = temp;
+}
+/**
+ * Code description: Prints the array after each swap
+ * bubble_sort - it sorts an array of integers in ascending order
+ * @array: An array of integers to be sorted
+ * @size: Which is the size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t x, y;
+int temp;
+for (x = 0; x < size - 1; x++)
+{
+for(y = 0; y < size - 1 - x; y++)
+{
+if (array[y] > array[y + 1])
+{
+temp = array[y];
+array[y] = array[y+1];
+array[y+1] = temp;
+}
+}
+}
 }
