4c49fb219712ee5bff0069b53adb78c6cf3bac22
Soft-Mahmood <softmahmood@gmail.com>
2023-09-28 12:58:32 -0700

    radix

--- sort.h
+++ sort.h
@@ -49,8 +49,11 @@ void cocktail_sort_list(listint_t **list);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
-void swap_ints(int *a, int *b);
 void max_heapify(int *array, size_t size, size_t base, size_t root);
 void heap_sort(int *array, size_t size);
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+
 
 #endif
